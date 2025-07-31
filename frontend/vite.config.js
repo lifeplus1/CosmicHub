5df694a639b4a74c012e8625277373381529e371
lifeplus1 <chrisguida92@gmail.com>
2025-07-31 00:51:10 -0400

    vite.config fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Explicitly set output directory to 'dist'
+    sourcemap: true // Optional: for debugging
+  },
   test: {
     environment: 'jsdom',
     setupFiles: './src/setupTests.js',
