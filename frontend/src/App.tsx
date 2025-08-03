338e8573ada5e601d5bec0ccaf4bc9b6b7800dd2
lifeplus1 <chrisguida92@gmail.com>
2025-08-03 18:07:26 -0400

    AuthContext fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,47 +1,10 @@
 // frontend/src/App.tsx
 import React, { useState } from "react";
 import type { ChangeEvent, FormEvent } from "react";
-import {
-  ChakraProvider,
-  Box,
-  Heading,
-  FormControl,
-  FormLabel,
-  Input,
-  Select,
-  Button,
-  VStack,
-  Text,
-  useToast,
-} from "@chakra-ui/react";
-import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
-import axios from "axios";
-import ChartDisplay from "./components/ChartDisplay.js";
-import Login from "./components/Login.js";
-import Signup from "./components/Signup.js";
-import Navbar from "./components/Navbar.js";
-import Footer from "./components/Footer.js";
-import AnalyzePersonality from "./components/AnalyzePersonality.js";
-import AIChat from "./components/AIChat.js";
-import { AuthProvider, useAuth } from "./components/AuthProvider.js";
-import SavedCharts from "./components/SavedCharts.js";
-import { getFirestore, collection, addDoc } from "firebase/firestore";
-import { getAuthToken } from "./lib/auth";
 
-interface ChartData {
-  latitude: number;
-  longitude: number;
-  timezone: string;
-  julian_day: number;
-  planets: Record<string, { position: number; retrograde: boolean }>;
-  houses: Array<{ house: number; cusp: number }>;
-  angles: Record<string, number>;
-  aspects: Array<any>;
-}
-
-const App: React.FC = () => {
+function MainApp() {
   const authContext = useAuth();
-  console.log('AuthContext value in App:', authContext);
+  console.log('AuthContext value in MainApp:', authContext);
   const { user } = authContext;
   const [birthData, setBirthData] = useState({
     date: "",
@@ -119,41 +82,86 @@ const App: React.FC = () => {
   };
 
   return (
+    <>
+      <Navbar />
+      <Box p={4}>
+        <Routes>
+          <Route
+            path="/"
+            element={(
+              <VStack spacing={4}>
+                <Heading color="gold">Astrology Chart Calculator</Heading>
+                <form onSubmit={handleSubmit}>
+                  <FormControl isRequired>
+                    <FormLabel color="yellow.200">Date of Birth</FormLabel>
+                    <Input
+                      type="date"
+                      name="date"
+                      value={birthData.date}
+                      onChange={handleInputChange}
+                      bg="purple.700"
+                      color="white"
+                      borderColor="gold"
+                    />
+                  </FormControl>
+                  <FormControl isRequired>
+                    <FormLabel color="yellow.200">Time of Birth</FormLabel>
+                    <Input
+                      type="time"
+                      name="time"
+                      value={birthData.time}
+                      onChange={handleInputChange}
+                      bg="purple.700"
+                      color="white"
+                      borderColor="gold"
+                    />
+                  </FormControl>
+                  <FormControl isRequired>
+                    <FormLabel color="yellow.200">City</FormLabel>
+                    <Input
+                      type="text"
+                      name="city"
+                      value={birthData.city}
+                      onChange={handleInputChange}
+                      bg="purple.700"
+                      color="white"
+                      borderColor="gold"
+                    />
+                  </FormControl>
+                  <Button type="submit" colorScheme="teal" isLoading={loading} w="100%">
+                    Calculate Chart
+                  </Button>
+                </form>
+                {error && <Text color="red.500">{error}</Text>}
+                {chart && <ChartDisplay chart={chart} />}
+                {!user && (
+                  <Button colorScheme="yellow" variant="solid" w="100%" mt={4} onClick={() => navigate("/signup")}>Sign Up for an Account</Button>
+                )}
+              </VStack>
+            )}
+          />
+          <Route path="/saved-charts" element={user ? <SavedCharts /> : <Navigate to="/login" />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Box>
+      <Footer />
+    </>
+  );
+}
+
+const App: React.FC = () => {
+  return (
     <AuthProvider>
       <ChakraProvider>
-        <Navbar />
-        <Box p={4}>
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <VStack spacing={4}>
-                  <Heading color="gold">Astrology Chart Calculator</Heading>
-                  <form onSubmit={handleSubmit}>
-                    <FormControl isRequired>
-                      <FormLabel color="yellow.200">Date of Birth</FormLabel>
-                      <Input
-                        type="date"
-                        name="date"
-                        value={birthData.date}
-                        onChange={handleInputChange}
-                        bg="purple.700"
-                        color="white"
-                        borderColor="gold"
-                      />
-                    </FormControl>
-                    <FormControl isRequired>
-                      <FormLabel color="yellow.200">Time of Birth</FormLabel>
-                      <Input
-                        type="time"
-                        name="time"
-                        value={birthData.time}
-                        onChange={handleInputChange}
-                        bg="purple.700"
-                        color="white"
-                        borderColor="gold"
-                      />
-                    </FormControl>
+        <MainApp />
+      </ChakraProvider>
+    </AuthProvider>
+  );
+};
+
+export default App;
                     <FormControl isRequired>
                       <FormLabel color="yellow.200">City</FormLabel>
                       <Input
